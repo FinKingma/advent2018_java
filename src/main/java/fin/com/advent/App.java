049d20c16adb0a11e6a0061779e66e7d3ccb226a
finkingma <finkingma@gmail.com>
2019-02-22 09:55:50 +0100

    puzzle 2 completed

--- src/main/java/fin/com/advent/App.java
+++ src/main/java/fin/com/advent/App.java
@@ -16,6 +16,7 @@ public class App
     public static void main( String[] args )
     {
     	List<String> allLines = new ArrayList<String>();
+    	List<Integer> historyLines = new ArrayList<Integer>();
     	
     	try {
 			allLines = Files.readAllLines(Paths.get("/Users/finkingma/eclipse-workspace/advent/input.txt"));
@@ -24,12 +25,24 @@ public class App
 		}
 
     	Integer number = 0;
+    	boolean finished = false;
+    	Integer attempts = 0;
+    	historyLines.add(number);
     	
-    	for (String item : allLines) {
-    		Integer update = Integer.parseInt(item);
-    		number += update;
+    	do {
+	    	for (String item : allLines) {
+	    		Integer update = Integer.parseInt(item);
+	    		number += update;
+	    		
+	    		if (historyLines.contains(number)) {
+	    			System.out.println( "answer is: " + number );
+	    			finished = true;
+	    			break;
+	    		}
+	    		historyLines.add(number);
+	    		attempts ++;
+	    	}
     	}
-    	
-        System.out.println( "answer is: " + number );
+	    while (finished == false && attempts < 1000000);
     }
 }
